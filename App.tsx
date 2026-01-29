53ba7a3f093499d782a1960a1843e6ece29c5bbc
Savita20111991 <savita20111991@gmail.com>
2026-01-30 02:52:51 +0530

    Enable routing for all pages

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import ExportMarkets from "./pages/ExportMarkets";
+import Quality from "./pages/Quality";
+import Contact from "./pages/Contact";
+import AdminPanel from "./pages/AdminPanel";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/export-markets" element={<ExportMarkets />} />
+        <Route path="/quality" element={<Quality />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
